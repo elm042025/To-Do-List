c5486a18f220180075b11dad520a6ccb8264e700
Mido <elm042025@gmail.com>
2025-07-02 11:45:32 +0200

    Add structure and styling to To-Do App, including header, form, and task list; update credits and add icons

---
+++ script.js
@@ -0,0 +1,12 @@
+//! ---------- New task input and new task button ---------- //
+
+const newTaskInput = document.querySelector("#newTaskInput");
+const addTaskBtn = document.querySelector("#addTaskBtn");
+
+//! ----- P tag that updates content depending on the operation -----//
+
+const liveText = document.querySelector("#liveText");
+
+//! ----- Saved Tasks List ----- //
+
+const savedTasksList = document.querySelector("#savedTasksUL");
